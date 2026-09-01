35f3e0a5b69e6992c74494936a55a662e1fcec0f
3dmaker1910 <asnmaker@gmail.com>
2026-09-01 14:32:41 -0500

    feat: add Flutter Android scaffold with signing config

---
+++ biomech_ai/android/app/build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    id("com.android.application")
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "com.biomechai.biomech_ai"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    defaultConfig {
+        applicationId = "com.biomechai.biomech_ai"
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    signingConfigs {
+        create("release") {
+            keyAlias = System.getenv("KEY_ALIAS") ?: "biomech_ai"
+            keyPassword = System.getenv("KEY_PASSWORD") ?: "biomech2024"
+            storeFile = file("biomech_ai.keystore")
+            storePassword = System.getenv("STORE_PASSWORD") ?: "biomech2024"
+        }
+    }
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("release")
+            isMinifyEnabled = false
+            isShrinkResources = false
+        }
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
+    }
+}
+
+flutter {
+    source = "../.."
+}
---
+++ biomech_ai/android/app/src/main/kotlin/com/biomechai/biomech_ai/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.biomechai.biomech_ai
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
